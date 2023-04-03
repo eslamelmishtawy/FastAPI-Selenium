dfb01f06ec15a038faaaab14eeb834d0e2919caa
Eslam Elmishtawy <eslamelmishtawy@gmail.com>
2023-04-03 14:28:46 +0200

    test

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI, BackgroundTasks, HTTPException
 from pydantic import BaseModel
 from extract import *
 import os
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 SECRET = os.getenv("SECRET")
 
 #
 app = FastAPI()
+
+urls = [
+    "https://radiant-hummingbird-697a83.netlify.app/",
+    "http://trialserver.rf.gd/trial6/www.classcentral.com/index.html",
+    "https://class-central.vercel.app/www.classcentral.com/index.html",
+    "https://5dcookie.github.io/",
+    "https://www.census2011.co.in/city.php#:~:text=Here%20are%20the%20details%20of,%2D%203%2C124%2C458%2C%20Jaipur%20%2D%203%2C046%2C163",
+]
 
 class Msg(BaseModel):
     msg: str
@@ -16,22 +26,12 @@ class Msg(BaseModel):
 @app.get("/")
 
 async def root():
-    return {"message": "Hello World. Welcome to Eslam!"}
-
-
-@app.get("/homepage")
-async def demo_get():
-    driver=createDriver()
-
-    homepage = getGoogleHomepage(driver)
-    driver.close()
-    return homepage
-
-@app.post("/backgroundDemo")
-async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
-    
-    background_tasks.add_task(doBackgroundTask, inp)
-    return {"message": "Success, background task started"}
-    
+    driver = webdriver.Firefox()
+    for i in urls:
+        driver.get(i)
+        webpage_lang = driver.find_element(By.XPATH, "//html[@lang='hi']")
+        if (webpage_lang.is_displayed()):
+            return "pass"
+        return "fail"
 
 
